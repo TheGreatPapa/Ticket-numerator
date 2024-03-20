8df0babc117ca4ae45282f3bf3c1c81bb051ee2c
TheGratePapa <luisk_lcm54z@hotmail.com>
2024-03-20 11:11:48 -0700

    botones que modifican etiquetas con el path del archivo seleccionado

--- main.py
+++ main.py
@@ -2,21 +2,29 @@ import customtkinter
 from customtkinter import filedialog
 
 def main():
-    print("si sirvio")
     ticket_GUI()
 
 
 
 class ticket_GUI:
     def __init__(self):
-        
+
         #initialize root
         self.root = customtkinter.CTk()
         self.root.geometry("800x500")
 
+        #text variables
+        self.text_template = customtkinter.StringVar()
+        self.text_output = customtkinter.StringVar()
+        self.text_image = customtkinter.StringVar()
+
+        #initialize text variables
+        self.text_template.set("No seleccionado")
+        self.text_output.set("No seleccionado")
+        self.text_image.set("No seleccionado")
 
         #Boton seleccion de imagen
-        self.btn_imagen = customtkinter.CTkButton(master=self.root,
+        self.btn_image = customtkinter.CTkButton(master=self.root,
                                                     text="Seleccionar ticket",
                                                     font=("Roboto",14),
                                                     command=self.select_image_file)
@@ -34,23 +42,34 @@ class ticket_GUI:
         self.btn_generate = customtkinter.CTkButton(master=self.root,
                                                     text="Generar",
                                                     font=("Roboto",14),
-                                                    command=self.generate_file)
+                                                    command=self.generate_file,
+                                                    fg_color="green")
         
-
         #etiquetas
-        #label = customtkinter.CTkLabel(master=frame, text="Login", font=("Roboto",24))
+        self.label_template = customtkinter.CTkLabel(master=self.root,
+                                                textvariable=self.text_template,
+                                                font=("Roboto",14))
+        
+        self.label_output = customtkinter.CTkLabel(master=self.root,
+                                                textvariable=self.text_output,
+                                                font=("Roboto",14))
+        
+        self.label_image = customtkinter.CTkLabel(master=self.root,
+                                                textvariable=self.text_image,
+                                                font=("Roboto",14))
         
 
         #checkboxes
-        check_letters = customtkinter.CTkCheckBox(master=self.root, text="Incluir letras?")
+        self.check_letters = customtkinter.CTkCheckBox(master=self.root, text="Incluir letras?")
 
         
         #posicionamiento de elementos 
-        
-        
-        #label.pack()
-        check_letters.pack(padx=10,pady=12)
-        self.btn_imagen.pack(padx=10,pady=12)
+
+        self.label_output.pack(padx=10,pady=12)
+        self.label_image.pack(padx=10,pady=12)
+        self.label_template.pack(padx=10,pady=12)
+        self.check_letters.pack(padx=10,pady=12)
+        self.btn_image.pack(padx=10,pady=12)
         self.btn_open_template.pack(padx=10,pady=12)
         self.btn_output_save.pack(padx=10,pady=12)
         self.btn_generate.pack(padx=10,pady=12)
@@ -61,17 +80,24 @@ class ticket_GUI:
     def select_image_file(self):
         self.image_path = filedialog.askopenfilename(title="Abrir archivo PNG",
                                                      filetypes=(("PNG","*.png"),("Todos los archivos (*.*)","*.*")))
-        print(self.image_path)
+        if self.image_path:
+            self.text_image.set(self.image_path)
+        #print(self.image_path)
 
     def select_saving_path(self):
         self.save_path = filedialog.askdirectory(title="Escoger lugar a guardar imágenes")
-        print(self.save_path)
+        self.text_output.set(self.save_path)
+        if self.save_path:
+            self.text_output.set(self.save_path)
+        #print(self.save_path)
 
     def select_template_file(self):
-        self.image_path = filedialog.askopenfilename(title="Abrir archivo xlsx",
+        self.template_path = filedialog.askopenfilename(title="Abrir archivo xlsx",
                                                      filetypes=(("Excel (*.xlsx)","*.xlsx"),("Todos los archivos (*.*)","*.*")))
         print(self.image_path)
-
+        if self.template_path:
+            self.text_template.set(self.template_path)
+            
     def generate_file(self):
         pass
 
