e9aed441cd421adeba4dadfeca3b96922820d0ef
TheGratePapa <luisk_lcm54z@hotmail.com>
2024-06-26 14:08:26 -0700

    added an example xlsx, fixed not showing file path if not selected, changed how it is selected a font: from dropdown menu to file selection

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import customtkinter
 from customtkinter import filedialog
 from pygame import font
+from pathlib import Path
 
 def fonts_getting():
     fonts = font.get_fonts()
@@ -25,11 +26,14 @@ class ticket_GUI:
         self.text_template = customtkinter.StringVar()
         self.text_output = customtkinter.StringVar()
         self.text_image = customtkinter.StringVar()
+        self.text_font = customtkinter.StringVar()
 
         #initialize text variables
         self.text_template.set("No seleccionado")
         self.text_output.set("No seleccionado")
         self.text_image.set("No seleccionado")
+        self.text_font.set("No seleccionado")
+        
 
         #Boton seleccion de imagen
         self.btn_image = customtkinter.CTkButton(master=self.root,
@@ -42,6 +46,11 @@ class ticket_GUI:
                                                     font=("Roboto",14),
                                                     command=self.select_saving_path)
         
+        self.btn_select_font = customtkinter.CTkButton(master=self.root,
+                                                    text="seleccionar font",
+                                                    font=("Roboto",14),
+                                                    command=self.select_font_file)
+        
         self.btn_open_template = customtkinter.CTkButton(master=self.root,
                                                     text="abrir plantilla",
                                                     font=("Roboto",14),
@@ -52,6 +61,8 @@ class ticket_GUI:
                                                     font=("Roboto",14),
                                                     command=self.generate_file,
                                                     fg_color="green")
+        
+        
         
         #etiquetas
         self.label_template = customtkinter.CTkLabel(master=self.root,
@@ -66,12 +77,17 @@ class ticket_GUI:
                                                 textvariable=self.text_image,
                                                 font=("Roboto",14))
         
+        self.label_font = customtkinter.CTkLabel(master=self.root,
+                                                textvariable=self.text_font,
+                                                font=("Roboto",14))
+        
 
         #checkboxes
-        self.check_letters = customtkinter.CTkCheckBox(master=self.root, text="Incluir letras?")
+        #self.check_letters = customtkinter.CTkCheckBox(master=self.root, text="Incluir letras?")
 
         #dropdown menu
-        self.dropdown_fonts = customtkinter.CTkComboBox(master=self.root,values=self.fonts)
+        #self.dropdown_fonts = customtkinter.CTkComboBox(master=self.root,values=self.fonts)
+       
         
         
         #posicionamiento de elementos 
@@ -79,12 +95,15 @@ class ticket_GUI:
         self.label_output.pack(padx=10,pady=12)
         self.label_image.pack(padx=10,pady=12)
         self.label_template.pack(padx=10,pady=12)
-        self.check_letters.pack(padx=10,pady=12)
+        self.label_font.pack(padx=10,pady=12)
+        #self.check_letters.pack(padx=10,pady=12)
         self.btn_image.pack(padx=10,pady=12)
+        self.btn_select_font.pack(padx=10,pady=12)
         self.btn_open_template.pack(padx=10,pady=12)
         self.btn_output_save.pack(padx=10,pady=12)
         self.btn_generate.pack(padx=10,pady=12)
-        self.dropdown_fonts.pack(padx=10,pady=12)
+        #self.dropdown_fonts.pack(padx=10,pady=12)
+       
         
 
         self.root.mainloop()
@@ -94,12 +113,13 @@ class ticket_GUI:
         self.image_path = filedialog.askopenfilename(title="Abrir archivo PNG",
                                                      filetypes=(("PNG","*.png"),("Todos los archivos (*.*)","*.*")))
         if self.image_path:
-            self.text_image.set(self.image_path)
+            display_image = Path(self.image_path).name
+            self.text_image.set(display_image)
         #print(self.image_path)
 
     def select_saving_path(self):
         self.save_path = filedialog.askdirectory(title="Escoger lugar a guardar imágenes")
-        self.text_output.set(self.save_path)
+    
         if self.save_path:
             self.text_output.set(self.save_path)
         #print(self.save_path)
@@ -107,9 +127,18 @@ class ticket_GUI:
     def select_template_file(self):
         self.template_path = filedialog.askopenfilename(title="Abrir archivo xlsx",
                                                      filetypes=(("Excel (*.xlsx)","*.xlsx"),("Todos los archivos (*.*)","*.*")))
-        print(self.image_path)
+        
         if self.template_path:
-            self.text_template.set(self.template_path)
+            display_template = Path(self.template_path).name
+            self.text_template.set(display_template)
+
+    def select_font_file(self):
+        self.font_path = filedialog.askopenfilename(title="Seleccionar archivo Font",
+                                                     filetypes=(("Font file (*.ttf)","*.ttf"),("Font file (*.TTF)","*.TTF"),("Todos los archivos (*.*)","*.*")))
+        print(self.font_path)
+        if self.font_path:
+            display_font = Path(self.font_path).name
+            self.text_font.set(display_font)
             
     def generate_file(self):
         pass
