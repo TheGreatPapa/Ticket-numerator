c11f4418bb6e25683bf2437489aee82c460f15cd
TheGratePapa <luisk_lcm54z@hotmail.com>
2024-06-26 16:30:44 -0700

    Separated buttons

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
-import customtkinter
+import customtkinter as ctk
 from customtkinter import filedialog
 from pygame import font
 from pathlib import Path
 
-def fonts_getting():
-    fonts = font.get_fonts()
-    fonts.sort()
-    return fonts
 
 
 def main():
@@ -17,92 +13,90 @@ def main():
 class ticket_GUI:
     def __init__(self):
 
-        self.fonts = fonts_getting()
         #initialize root
-        self.root = customtkinter.CTk()
+        self.root = ctk.CTk()
         self.root.geometry("800x500")
+        
 
-        #text variables
-        self.text_template = customtkinter.StringVar()
-        self.text_output = customtkinter.StringVar()
-        self.text_image = customtkinter.StringVar()
-        self.text_font = customtkinter.StringVar()
+        # Text variables
+        self.text_template = ctk.StringVar()
+        self.text_output = ctk.StringVar()
+        self.text_image = ctk.StringVar()
+        self.text_font = ctk.StringVar()
 
-        #initialize text variables
+        # Initialize text variables
         self.text_template.set("No seleccionado")
         self.text_output.set("No seleccionado")
         self.text_image.set("No seleccionado")
         self.text_font.set("No seleccionado")
-        
 
-        #Boton seleccion de imagen
-        self.btn_image = customtkinter.CTkButton(master=self.root,
-                                                    text="Seleccionar ticket",
-                                                    font=("Roboto",14),
-                                                    command=self.select_image_file)
-        
-        self.btn_output_save = customtkinter.CTkButton(master=self.root,
-                                                    text="Seleccionar carpeta destino",
-                                                    font=("Roboto",14),
-                                                    command=self.select_saving_path)
-        
-        self.btn_select_font = customtkinter.CTkButton(master=self.root,
-                                                    text="seleccionar font",
-                                                    font=("Roboto",14),
-                                                    command=self.select_font_file)
-        
-        self.btn_open_template = customtkinter.CTkButton(master=self.root,
-                                                    text="abrir plantilla",
-                                                    font=("Roboto",14),
-                                                    command=self.select_template_file)
-        
-        self.btn_generate = customtkinter.CTkButton(master=self.root,
-                                                    text="Generar",
-                                                    font=("Roboto",14),
-                                                    command=self.generate_file,
-                                                    fg_color="green")
-        
-        
-        
-        #etiquetas
-        self.label_template = customtkinter.CTkLabel(master=self.root,
-                                                textvariable=self.text_template,
-                                                font=("Roboto",14))
-        
-        self.label_output = customtkinter.CTkLabel(master=self.root,
-                                                textvariable=self.text_output,
-                                                font=("Roboto",14))
-        
-        self.label_image = customtkinter.CTkLabel(master=self.root,
-                                                textvariable=self.text_image,
-                                                font=("Roboto",14))
-        
-        self.label_font = customtkinter.CTkLabel(master=self.root,
-                                                textvariable=self.text_font,
-                                                font=("Roboto",14))
-        
+        # Frame for grid-managed widgets
+        self.frame = ctk.CTkFrame(self.root)
+        self.frame.pack(padx=10, pady=12)
 
-        #checkboxes
-        #self.check_letters = customtkinter.CTkCheckBox(master=self.root, text="Incluir letras?")
-
-        #dropdown menu
-        #self.dropdown_fonts = customtkinter.CTkComboBox(master=self.root,values=self.fonts)
-       
+        # Buttons
+        self.btn_image = ctk.CTkButton(master=self.frame,
+                                       text="Seleccionar ticket",
+                                       font=("Roboto", 14),
+                                       command=self.select_image_file)
         
+        self.btn_output_save = ctk.CTkButton(master=self.frame,
+                                             text="Seleccionar carpeta destino",
+                                             font=("Roboto", 14),
+                                             command=self.select_saving_path)
         
-        #posicionamiento de elementos 
+        self.btn_select_font = ctk.CTkButton(master=self.frame,
+                                             text="seleccionar font",
+                                             font=("Roboto", 14),
+                                             command=self.select_font_file)
+        
+        self.btn_open_template = ctk.CTkButton(master=self.frame,
+                                               text="abrir plantilla",
+                                               font=("Roboto", 14),
+                                               command=self.select_template_file)
+        
+        # Labels
+        self.label_template = ctk.CTkLabel(master=self.frame,
+                                           textvariable=self.text_template,
+                                           font=("Roboto", 14))
+        
+        self.label_output = ctk.CTkLabel(master=self.frame,
+                                         textvariable=self.text_output,
+                                         font=("Roboto", 14))
+        
+        self.label_image = ctk.CTkLabel(master=self.frame,
+                                        textvariable=self.text_image,
+                                        font=("Roboto", 14))
+        
+        self.label_font = ctk.CTkLabel(master=self.frame,
+                                       textvariable=self.text_font,
+                                       font=("Roboto", 14))
+        
+        # Positioning elements with grid
+        self.btn_output_save.grid(row=0, column=0, padx=10, pady=12)
+        self.label_output.grid(row=0, column=1, padx=10, pady=12)
+        
+        self.btn_image.grid(row=1, column=0, padx=10, pady=12)
+        self.label_image.grid(row=1, column=1, padx=10, pady=12)
+        
+        self.btn_select_font.grid(row=2, column=0, padx=10, pady=12)
+        self.label_font.grid(row=2, column=1, padx=10, pady=12)
+        
+        self.btn_open_template.grid(row=3, column=0, padx=10, pady=12)
+        self.label_template.grid(row=3, column=1, padx=10, pady=12)
 
-        self.label_output.pack(padx=10,pady=12)
-        self.label_image.pack(padx=10,pady=12)
-        self.label_template.pack(padx=10,pady=12)
-        self.label_font.pack(padx=10,pady=12)
-        #self.check_letters.pack(padx=10,pady=12)
-        self.btn_image.pack(padx=10,pady=12)
-        self.btn_select_font.pack(padx=10,pady=12)
-        self.btn_open_template.pack(padx=10,pady=12)
-        self.btn_output_save.pack(padx=10,pady=12)
-        self.btn_generate.pack(padx=10,pady=12)
-        #self.dropdown_fonts.pack(padx=10,pady=12)
+        # Create a new frame for the generate button
+        self.frame_generate = ctk.CTkFrame(self.root)
+        self.frame_generate.pack(padx=10, pady=12, fill="x")
+
+        self.btn_generate = ctk.CTkButton(master=self.frame_generate,
+                                          text="Generar",
+                                          font=("Roboto", 14),
+                                          command=self.generate_file,
+                                          fg_color="green")
+        self.btn_generate.pack(padx=10, pady=12)
+
+        self.root.mainloop()
        
         
 
